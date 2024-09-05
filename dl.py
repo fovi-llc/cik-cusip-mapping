10c730539a675dc66f06008dfcd57bcb368b89e3
Jim White <jim@ifcx.org>
2024-09-04 23:02:59 -0700

    Use filtered index file.

--- dl.py
+++ dl.py
@@ -1,26 +1,16 @@
-import argparse
 import csv
 
 from downloader import download_files
 
-from main_parameters import SEC_ARCHIVES_URL, ARCHIVES_FOLDER
+from main_parameters import SEC_ARCHIVES_URL, ARCHIVES_FOLDER, FILTERED_INDEX_FILE
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filing", type=str)
-    parser.add_argument("folder", type=str)
-
-    args = parser.parse_args()
-    filing = args.filing
-    folder = args.folder
-
     rows = []
-    with open("full_index.csv", "r") as f:
+    with open(FILTERED_INDEX_FILE, "r") as f:
         reader = csv.DictReader(f)
         for row in reader:
-            if filing in row["form"]:
-                row["file_path"] = ARCHIVES_FOLDER / row["url"]
-                row["url"] = SEC_ARCHIVES_URL + row['url']
-                rows.append(row)
+            row["file_path"] = ARCHIVES_FOLDER / row["filename"]
+            row["url"] = SEC_ARCHIVES_URL + row["filename"]
+            rows.append(row)
 
     download_files(rows)
