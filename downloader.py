4ae23c47ce0189f3dd35e6471ab3bb9cc36747dd
Jim White <jim@ifcx.org>
2024-09-04 23:02:40 -0700

    Use filtered index file.

--- downloader.py
+++ downloader.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python
 import requests
-
-from concurrent.futures import ProcessPoolExecutor
 from ratelimit import limits, sleep_and_retry
+
 import time
-import logging
-import multiprocessing
 from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
+import logging
 from logging.handlers import QueueHandler, QueueListener
+
 from typing import Sequence
 
-from main_parameters import SEC_HTTP_HEADERS, HTTP_TIMEOUT, SEC_RATE_LIMIT, MAX_CHUNKSIZE
+
+from main_parameters import (
+    SEC_HTTP_HEADERS,
+    HTTP_TIMEOUT,
+    SEC_RATE_LIMIT,
+    # MAX_CHUNKSIZE,
+)
 
 _logger_name = "sec_edgar_dl"
 
@@ -22,21 +28,19 @@ def download_file(row):
     logger = logging.getLogger(_logger_name)
     try:
         logger.info(f"Working on item: {row=}")
-        url = row['url']
-        response = requests.get(
-            url=url, headers=SEC_HTTP_HEADERS, timeout=HTTP_TIMEOUT
-        )
+        url = row["url"]
+        response = requests.get(url=url, headers=SEC_HTTP_HEADERS, timeout=HTTP_TIMEOUT)
         while "Request Rate Threshold Exceeded" in response.text:
-            logger.warning("Rate limit exceeded. Waiting for 615 seconds.")
+            logger.warning(f"Rate limit exceeded. Waiting at {row=}")
             time.sleep(615)
             response = requests.get(
                 url=url, headers=SEC_HTTP_HEADERS, timeout=HTTP_TIMEOUT
             )
         if response.status_code != 200:
             logger.warning(f"Expected 200 but got {response.status_code=} for {row=}")
-        file_path = row['file_path']
+        file_path = row["file_path"]
         file_path.parent.mkdir(parents=True, exist_ok=True)
-        with open(file_path, 'wb') as fd:
+        with open(file_path, "wb") as fd:
             for chunk in response.iter_content(chunk_size=4096):
                 fd.write(chunk)
     except Exception as e:
@@ -55,7 +59,7 @@ def init_worker(log_queue: multiprocessing.Queue, log_level: int) -> None:
 def download_files(rows: Sequence, overwrite: bool = False) -> None:
     # gwerbin/multiprocessing_logging.py
     # https://gist.github.com/gwerbin/e9ab7a88fef03771ab0bf3a11cf921bc
-    log_level = logging.INFO
+    log_level = logging.WARNING
     log_format = "%(name)s:%(levelname)s:%(processName)s:%(asctime)s: %(message)s"
     formatter = logging.Formatter(log_format)
     stdout_handler = logging.StreamHandler()
@@ -82,11 +86,15 @@ def download_files(rows: Sequence, overwrite: bool = False) -> None:
         print("Overwriting existing files (if any).")
     else:
         print(f"Checking {len(rows)=} for existing files.")
-        rows = [row for row in rows if not row['file_path'].exists()]
+        rows = [row for row in rows if not row["file_path"].exists()]
 
     print(f"starting download of {len(rows)=} files.")
 
-    with ProcessPoolExecutor(max_workers=SEC_RATE_LIMIT, initializer=init_worker, initargs=(log_queue, log_level)) as executor:
+    with ProcessPoolExecutor(
+        max_workers=SEC_RATE_LIMIT,
+        initializer=init_worker,
+        initargs=(log_queue, log_level),
+    ) as executor:
         executor.map(
             download_file,
             rows,
@@ -98,5 +106,5 @@ def download_files(rows: Sequence, overwrite: bool = False) -> None:
     # # error handling here, or subclass QueueListener to behave differently.
     # log_listener.enqueue_sentinel()
     log_listener.stop()
-    
+
     print("Download complete.")
